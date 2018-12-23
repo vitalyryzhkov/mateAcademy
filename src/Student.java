f0aacfee76a67f54d8b5aef0e8c660434cf4cfcd
Vitaliy <pentagon0555@gmail.com>
2018-12-23 17:58:38 +0200

    test commit

--- src/Student.java
+++ src/Student.java
@@ -1,34 +1,23 @@
-import java.util.Scanner;
 
 public class Student {
     private String name;
     private int age;
     private String surname;
-    private int id;
-    static int counter = 1;
+    private double liderstvo = Math.random();
+    private double communikabelnost = Math.random();
+    private long id = System.currentTimeMillis();
 
     public Student() {
-        id = counter++;
+
     }
+
 
     public Student(String name, String surname, int age) {
         this.name = name;
         this.age = age;
         this.surname = surname;
-        id = counter++;
+
     }
-
-//    public void addStudent(Student student) {
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Enter name at new student: ");
-//        student.setName(scanner.next());
-//        System.out.println("Enter surname at new student: ");
-//        student.setSurname(scanner.next());
-//        System.out.println("Enter age at new student: ");
-//        student.setAge(scanner.nextInt());
-//    }
-
-
 
     public String getName() {
         return name;
@@ -44,10 +33,6 @@ public class Student {
 
     public void setSurname(String surname) {
         this.surname = surname;
-    }
-
-    public int getId() {
-        return id;
     }
 
     public int getAge() {

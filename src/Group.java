62b448d5855ad8c1de9e7c50eb17f884517e9cee
Vitaliy <pentagon0555@gmail.com>
2018-12-24 23:44:11 +0200

    maxElem

--- src/Group.java
+++ src/Group.java
@@ -2,15 +2,26 @@ import java.util.LinkedList;
 
 public class Group {
 
-    LinkedList<Student> studentLinkedList = new LinkedList<>();
+    private LinkedList<Student> studentLinkedList = new LinkedList<>();
 
     public void addStudent(Student student) {
         studentLinkedList.add(student);
-
     }
 
     public void seeStudents() {
         studentLinkedList.forEach(System.out::println);
+    }
+
+    void maxSumSkills() {
+        double max = 0.0;
+        for (int i = 0; i < studentLinkedList.size(); i++) {
+            for (int j = 0; j < studentLinkedList.size(); j++) {
+                if (studentLinkedList.get(i).getSumSkills() > studentLinkedList.get(j).getSumSkills()) {
+                    max = studentLinkedList.get(i).getSumSkills();
+                } else max = studentLinkedList.get(j).getSumSkills();
+            }
+        }
+        System.out.println(max);
     }
 }
 

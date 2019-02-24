4654f16b61cd154bdb8eaabb82446d56b2ecb833
Vitaliy <pentagon0555@gmail.com>
2019-02-24 15:05:25 +0200

    fixed

--- src/lesson01/Task1_4.java
+++ src/lesson01/Task1_4.java
@@ -14,7 +14,7 @@ public class Task1_4 {
     }
 
     private static long factorial(int number) {
-        if (number == 1) return 1;
+        if (number == 1 || number == 0) return 1;
 
         return number * factorial(number - 1);
     }

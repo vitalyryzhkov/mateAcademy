9d6486ef563884f58afb4474fc8202f98f64332d
Vitaliy <pentagon0555@gmail.com>
2019-02-27 00:19:41 +0200

    immutable done

---
+++ src/lesson02/task2_4/Main.java
@@ -0,0 +1,8 @@
+package lesson02.task2_4;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Joy", 18);
+        System.out.println(person.getName() + " " + person.getAge());
+    }
+}
---
+++ src/lesson02/task2_4/Person.java
@@ -0,0 +1,19 @@
+package lesson02.task2_4;
+
+public final class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}

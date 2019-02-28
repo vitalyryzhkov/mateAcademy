76a2c1775ad14a360658dedcbf3935df89c427bf
Vitaliy <pentagon0555@gmail.com>
2019-02-28 19:32:45 +0200

    first commit main branch

---
+++ src/lesson03/TestFork.java
@@ -0,0 +1,11 @@
+package lesson03;
+
+public class TestFork {
+    public static void main(String[] args) {
+        showInformation("Test fork git");
+    }
+
+    public static void showInformation(String string) {
+        System.out.println(string);
+    }
+}

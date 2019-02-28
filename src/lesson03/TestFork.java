bce90129d72ea4b962011557aae7656b78b8dd0d
Vitaliy <pentagon0555@gmail.com>
2019-02-28 19:43:06 +0200

    third commit second branch

--- src/lesson03/TestFork.java
+++ src/lesson03/TestFork.java
@@ -2,7 +2,7 @@ package lesson03;
 
 public class TestFork {
     public static void main(String[] args) {
-        showInformation("Second test fork git");
+        showInformation("Third test fork git");
     }
 
     private static void showInformation(String string) {
